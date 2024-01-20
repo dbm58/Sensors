fa1731db03160df58f9bd7c90b542010a5fcadb7
Dennis Meilicke <2196445+dbm58@users.noreply.github.com>
2024-01-20 13:57:24 -0600

    move govee to gails

--- bleson/run.sh
+++ bleson/run.sh
@@ -2,7 +2,7 @@
 
 macs=A4:C1:38:43:61:0E           # ATC_43610E - carol
 macs=$macs,A4:C1:38:29:CA:D6     # ATC_29CAD6 - cold frame
-macs=$macs,A4:C1:38:E4:43:C9     # Govee
+#macs=$macs,A4:C1:38:E4:43:C9     # Govee
 
 pushd `dirname $BASH_SOURCE`
 
