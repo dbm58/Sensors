694765fff090acf2ac3751c0d4da303600f459e8
Dennis Meilicke <2196445+dbm58@users.noreply.github.com>
2024-12-22 18:11:51 -0600

    WIP

--- bleak/run.sh
+++ bleak/run.sh
@@ -2,8 +2,8 @@
 
 macs=
 macs=$macs aranet4:DC:44:81:C5:E7:73    # CO2 Monitor
-macs=$macs lywsd03:A4:C1:38:43:61:0E    # ATC_43610E - carol
-macs=$macs lywsd03:A4:C1:38:29:CA:D6    # ATC_29CAD6 - cold frame
+#macs=$macs lywsd03:A4:C1:38:43:61:0E    # ATC_43610E - carol
+#macs=$macs lywsd03:A4:C1:38:29:CA:D6    # ATC_29CAD6 - cold frame
 #macs=$macs govee:A4:C1:38:E4:43:C9     # Govee
 
 pushd `dirname $BASH_SOURCE`
